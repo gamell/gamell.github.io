26e04f61f965a79b4a8504ecb5f0b011a46566b6
Joan Gamell <gamell@gamell.io>
2018-07-14 13:48:56 -0700

    bunch of changes, trying to create a dev environment

--- cljs/project.clj
+++ cljs/project.clj
@@ -1,70 +1,72 @@
 (defproject simple "0.9.0"
-  :dependencies [[org.clojure/clojure        "1.8.0"]
-                 [org.clojure/clojurescript  "1.9.908"]
-                 [reagent  "0.7.0"]
-                 [re-frame "0.10.1"]
-                 [cljs-ajax "0.7.2"]]
+  :dependencies [[org.clojure/clojure        "1.9.0"]
+                 [org.clojure/clojurescript  "1.10.339"]
+                 [reagent  "0.8.1"]
+                 [re-frame "0.10.5"]
+                 [cljs-ajax "0.7.4"]]
 
   :plugins [[lein-cljsbuild "1.1.5"]
             [lein-figwheel  "0.5.13"]
             [lein-less "1.7.5"]
             [lein-resource "16.9.1"]
             [lein-auto "0.1.3"]
-            [lein-cooper "1.2.2"]]
+            [lein-cooper "1.2.2"]
+            [lein-ancient "0.6.15"]]
 
   :hooks [leiningen.less leiningen.resource leiningen.cljsbuild]
 
-  :profiles {:dev {:dependencies [[binaryage/devtools "0.9.4"]
+  :repl-options {:port 12345}
+
+  :profiles {:dev {:dependencies [[binaryage/devtools "0.9.10"]
                                   [org.clojars.stumitchell/clairvoyant "0.2.1"]
-                                  [day8/re-frame-tracer "0.1.1-SNAPSHOT"]
-                                  [proto-repl "0.3.1"]]
+                                  [day8/re-frame-tracer "0.1.1-SNAPSHOT"]]
                    :figwheel {:repl false}
-                   :cljsbuild {:builds {:client {:figwheel {:on-jsload "gamell.core/run"}
-                                                 :compiler {:main "gamell.core"
+                   :cljsbuild {:builds {:client {:figwheel {:on-jsload "gamell.dev/run"}
+                                                 :source-paths ["src" "dev-src"]
+                                                 :compiler {:main "gamell.dev"
                                                             :asset-path "js"
                                                             :optimizations :none
                                                             :source-map true
                                                             :source-map-timestamp true
                                                             :closure-defines {"clairvoyant.core.devmode" true}
                                                             :preloads [devtools.preload]}}}}}
-
-             :min {:figwheel {:repl false}
-                   :cljsbuild {:builds {:client {:compiler {:optimizations :advanced
-                                                                :elide-asserts true
-                                                                :pretty-print false}}}}}
-
-             :build {:less {:target-path "build/css"}
-                     :resource {:target-path "build"}
-                     :cljsbuild {:builds {:client {:compiler {:output-to    "build/js/client.js"
-                                                              :closure-defines {"env.prod" true}}}}}}}
+             
+             :prod {:less {:target-path "build/css"}
+                    :resource {:target-path "build"}
+                    :cljsbuild {:builds {:client {:source-paths ["src"]
+                                                  :compiler {:output-to "build/js/client.js"
+                                                             :asset-path "js"
+                                                             :source-map false
+                                                             :optimizations :advanced
+                                                             :pretty-print false
+                                                             :elide-asserts true}}}}}}
 
   :figwheel {:css-dirs ["resources/public/css"]}
 
   :cooper {"figwheel" ["lein" "figwheel"]
-           "less" ["lein" "less" "auto"]          ;; LESS file watcher
+           "less" ["lein" "less" "auto"]           ;; LESS compiler & file watcher
            "reource"  ["lein" "auto" "resource"]}  ;; HTML Watcher
 
 
   :clean-targets ^{:protect false} ["resources/public" "build"]
 
-  :cljsbuild {:builds {:client {:source-paths ["src"]
-                                :compiler     {:output-dir "resources/public/js"
+  :cljsbuild {:builds {:client {:compiler     {:output-dir "resources/public/js"
                                                :output-to  "resources/public/js/client.js"}}}}
 
   :less {:source-paths ["resources/src/less"]
          :target-path "resources/public/css"}
 
-  :resource { :resource-paths ["resources/src"]
-              :target-path "resources/public"
-              :excludes [ #".+less"]}
+  :resource {:resource-paths ["resources/src"]
+             :target-path "resources/public"
+             :excludes [#".+less"]}
 
   :auto {:default {:paths ["resources/src"]
                    :file-pattern #"\.(html)$"}}
 
   :aliases {"dev"  ["with-profile" "dev" "cooper"]
-            "dev-min" ["with-profile" "min,dev" "cooper"]
-            "build" ["with-profile" "min,build" "do"
+            "dev-min" ["with-profile" "prod" "cooper"]
+            "build" ["with-profile" "prod" "do"
                      ["clean"]
                      ["less" "once"]
                      ["resource"]
-                     ["cljsbuild" "once"]]})
+                     ["cljsbuild" "once" "client"]]})
--- cljs/src/gamell/core.cljs
+++ cljs/src/gamell/core.cljs
@@ -2,13 +2,10 @@
   (:require [reagent.core :as reagent]
             [re-frame.core :as rf]
             [clojure.string :as str]
-            [ajax.core :refer [GET]]
-            [clairvoyant.core :refer-macros [trace-forms]]
-            [re-frame-tracer.core :refer [tracer]]))
+            [ajax.core :refer [GET]]))
 
 (def content-url "https://s3.amazonaws.com/gamell-io/data.json")
-(trace-forms {:tracer (tracer :color "green")}
-
+    
   ;; -- Domino 1 - Event Dispatch -----------------------------------------------
 
   ; (defn dispatch-timer-event
@@ -21,19 +18,19 @@
   ;; created in the face of figwheel hot-reloading of this file.
   ; (defonce do-timer (js/setInterval dispatch-timer-event 1000))
 
-  (def initial-content
-    {:pictures    ["first photo" "second photo"]
-     :repos     ["repo1" "repo2"]
-     :articles  ["article1" "article2"]})
+;  (def initial-content
+;    {:pictures    ["first photo" "second photo"]
+;     :repos     ["repo1" "repo2"]
+;     :articles  ["article1" "article2"]})
 
   ;; -- Domino 2 - Event Handlers -----------------------------------------------
 
-  (rf/reg-event-db              ;; sets up initial application state
-    :initialize                 ;; usage:  (dispatch [:initialize])
-    (fn [_ _]                   ;; the two parameters are not important here, so use _
-      {:time (js/Date.)         ;; What it returns becomes the new application state
-       :time-color "#f88"       ;; so the application state will initially be a map with two keys
-       :content initial-content}))
+(rf/reg-event-db             ;; sets up initial application state
+  :initialize                 ;; usage:  (dispatch [:initialize])
+  (fn [_ _]                   ;; the two parameters are not important here, so use _
+    {:content {}}))           ;; What it returns becomes the new application state    
+                              ;; so the application state will initially be a map with two keys
+
 
 
   ; (rf/reg-event-db                ;; usage:  (dispatch [:time-color-change 34562])
@@ -42,19 +39,19 @@
   ;     (assoc db :time-color new-color-value)))   ;; compute and return the new application state
 
 
-  (defn load-content
-    []
-    (GET content-url
-      {:response-format :json
-       :keywords? true
-       :handler (fn [content]
-                  (js/console.log "*** CONTENT LOADED ***")
-                  (rf/dispatch [:content-loaded content]))}))
+(defn load-content
+   []
+   (GET content-url
+     {:response-format :json
+      :keywords? true
+      :handler (fn [content]
+                 (js/console.log "*** CONTENT LOADED ***")
+                 (rf/dispatch [:content-loaded content]))}))
 
-  (rf/reg-event-db                  ;; usage:  (dispatch [:time-color-change 34562])
-    :content-loaded
-    (fn [db [_ new-content]]
-      (assoc db :content (:data new-content))))
+(rf/reg-event-db                  ;; usage:  (dispatch [:time-color-change 34562])
+  :content-loaded
+  (fn [db [_ new-content]]
+    (assoc db :content (:data new-content))))
 
   ; (rf/reg-event-db                 ;; usage:  (dispatch [:timer a-js-Date])
   ;   :timer                         ;; every second an event of this kind will be dispatched
@@ -74,10 +71,10 @@
   ;   (fn [db _]
   ;     (:time-color db)))
 
-  (rf/reg-sub
-    :content
-    (fn [db _]
-      (:content db)))
+(rf/reg-sub
+  :content
+  (fn [db _]
+    (:content db)))
 
 
   ;; -- Domino 5 - View Functions ----------------------------------------------
@@ -99,50 +96,86 @@
   ;             :value @(rf/subscribe [:time-color])
   ;             :on-change #(rf/dispatch [:time-color-change (-> % .-target .-value)])}]])  ;; <---
 
-  (def card-types {
-                   :pictures {
-                              :title :name
-                              :body :imageUrl}
-                   :repos {
-                           :title :repo
-                           :body :description}
+  ; (def card-types {
+  ;   :pictures {
+  ;             :title :name
+  ;             :body :imageUrl}
+  ;   :repos {
+  ;           :title :repo
+  ;           :body :description}
+  ;   :articles {
+  ;             :title :title
+  ;             :body :content}})
 
-                   :articles {
-                              :title :title
-                              :body :content}})
+(defn picture-card
+   [card-info]
+   [:h4 (:name card-info)]
+   [:div.photo
+    [:img {:src (:imageUrl card-info)}]])
+
+(defn repo-card
+   [card-info]
+   [:div
+    [:h4 (:repo card-info)]
+    [:p (:description card-info)]])
+
+(defn article-card
+   [card-info]
+   [:div
+    [:h4 (:title card-info)
+     [:p (:content card-info)]]])
 
 
-  (defn card
-    [type card-info id]
-    ^{:key (str "update-card-" (name type) "-" id)}
-    [:li.update-card ((:title (get card-types :pictures)) card-info)])
+(def type-map {:pictures {:card picture-card :class "photography" :title "Latest pictures"}
+                :repos {:card repo-card :class "projects" :title "Favorite projects"}
+                :articles {:card article-card :class "articles" :title "Latest Articles"}})
 
-  (defn content-section
-    [type data id]
-    ^{:key (str "update-section-" (name type) "-" id)}
-    [:ul.update-section (str ":type -> " type)
-         (js/console.log type)
-     (map #(card type %1 %2) data (iterate inc 0))])
+(defn get-class
+   [type]
+   (:class (get type-map type)))
 
-  (defn page-content
-    []
-    [:div.app "Content"
-     (let [content @(rf/subscribe [:content])]
-       (map
-         (fn [[type data] id] (content-section type data id))
-         content
-         (iterate inc 0)))])
+(defn get-card
+   [type]
+   (:card (get type-map type)))
 
-  (defn ui
-    []
-    [page-content])
+(defn card
+   [type card-info id]
+   ^{:key (str "update-card-" (name type) "-" id)}
+   [:li.update-card ((get-card type) card-info)])
+
+(defn content-section
+   [type data id]
+   ^{:key (str "update-section-" (name type) "-" id)}
+   (let [class (get-class type)
+         title (:title (get type-map type))]
+     [:section
+      [:ul.update-section {:class class}
+       [:a {:name class}]
+       [:h3 title]
+      ; (js/console.log type)
+       (map #(card type %1 %2) data (iterate inc 0))]]))
+
+
+(defn page-content
+   []
+   [:div.app
+    (let [content @(rf/subscribe [:content])]
+      (map
+       (fn [[type data] id] (content-section type data id))
+       content
+       (iterate inc 0)))])
+
+(defn ui
+   []
+   [page-content])
 
 
   ;; -- Entry Point -------------------------------------------------------------
 
-  (defn ^:export run
-    []
-    (rf/dispatch-sync [:initialize])       ;; puts a value into application state
-    (load-content)
-    (reagent/render [ui]                   ;; mount the application's ui into '<div id="app" />'
-                    (js/document.getElementById "content"))))
+(defn ^:export run
+   []
+   (rf/dispatch-sync [:initialize])       ;; puts a value into application state
+   (load-content)
+   (reagent/render
+    [ui]                   ;; mount the application's ui into '<div id="app" />'
+    (js/document.getElementById "content")))
