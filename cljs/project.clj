2491dda85c37fca2ada2ab1b53f514c82a928fb9
Joan Gamell <gamell@gamell.io>
2020-07-30 21:39:27 -0700

    build, several fixes

--- cljs/project.clj
+++ cljs/project.clj
@@ -8,7 +8,7 @@
   :plugins [[lein-cljsbuild "1.1.5"]
             [lein-figwheel  "0.5.19"]
             [lein-less "1.7.5"]
-            [lein-resource "16.9.1"]
+            [lein-resource "17.06.1"]
             [lein-auto "0.1.3"]
             [lein-cooper "1.2.2"]
             [lein-ancient "0.6.15"]]
@@ -17,36 +17,36 @@
 
   :repl-options {:port 12345}
 
-  :profiles {:dev {:figwheel {:repl false}
+  :profiles {:dev {:dependencies [[binaryage/devtools "1.0.2"]
+                                  [org.clojars.stumitchell/clairvoyant "0.2.1"]
+                                  [day8/re-frame-tracer "0.1.1-SNAPSHOT"]]
+                   :figwheel {:repl false}
                    :cljsbuild {:builds {:client {:figwheel {:on-jsload "gamell.core/run"}
                                                  :source-paths ["src"]
                                                  :compiler {:main "gamell.core"
+                                                            :output-dir "resources/public/js"
+                                                            :output-to  "resources/public/js/client.js"
                                                             :asset-path "js"
                                                             :optimizations :none
                                                             :source-map true
-                                                            :source-map-timestamp true}}}}}
-             
+                                                            :source-map-timestamp true
+                                                            :closure-defines {"clairvoyant.core.devmode" true}
+                                                            :preloads [devtools.preload]}}}}}
+
              :prod {:less {:target-path "build/css"}
                     :resource {:target-path "build"}
                     :cljsbuild {:builds {:client {:source-paths ["src"]
                                                   :compiler {:output-to "build/js/client.js"
+                                                             :output-dir "build/js"
                                                              :asset-path "js"
-                                                             :source-map "/build/js/sourcemap.js"
+                                                             :source-map "build/js/sourcemap.js"
                                                              :optimizations :advanced
                                                              :pretty-print false
                                                              :elide-asserts true}}}}}}
 
   :figwheel {:css-dirs ["resources/public/css"]}
 
-  :cooper {"figwheel" ["lein" "figwheel"]
-           "less" ["lein" "less" "auto"]           ;; LESS compiler & file watcher
-           "reource"  ["lein" "auto" "resource"]}  ;; HTML Watcher
-
-
   :clean-targets ^{:protect false} ["resources/public" "build"]
-
-  :cljsbuild {:builds {:client {:compiler     {:output-dir "resources/public/js"
-                                               :output-to  "resources/public/js/client.js"}}}}
 
   :less {:source-paths ["resources/src/less"]
          :target-path "resources/public/css"}
@@ -58,7 +58,12 @@
   :auto {:default {:paths ["resources/src"]
                    :file-pattern #"\.(html)$"}}
 
-  :aliases {"dev"  ["with-profile" "dev" "cooper"]
+  :cooper {"reource"  ["lein" "auto" "resource"] ;; HTML & resource watcher
+           "figwheel" ["lein" "figwheel"] ;; JS compiler and more
+           "less" ["lein" "less" "auto"] ;; LESS compiler & file watcher
+           }
+
+  :aliases {"dev"  ["with-profile" "dev" "do" ["clean"] ["cooper"]]
             "dev-min" ["with-profile" "prod" "cooper"]
             "build" ["with-profile" "prod" "do"
                      ["clean"]
